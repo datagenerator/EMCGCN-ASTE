d1de4ee9f31b1e9eb9962401056e1490da6f3802
datagenerator <97384104+datagenerator@users.noreply.github.com>
2022-12-28 22:17:47 +0530

    Update run.sh

--- code/run.sh
+++ code/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=0 python main.py --mode train --dataset res16 --batch_size 16 --epochs 100 --model_dir savemodel/ --seed 2000 --pooling avg --prefix ../data/D2/ --wandb_project "MLRC EMCGCN" --wandb_run "EMCGCN res16 D2 seed 2000" --config config.yaml --wandb_api_key '10989cf7d666f7d3bfcc31c6185448bda1a19767'
+CUDA_VISIBLE_DEVICES=0 python main.py --mode train --dataset lap14 --batch_size 16 --epochs 100 --model_dir savemodel/ --seed 1000 --pooling avg --prefix ../data/D2/ --wandb_project "MLRC EMCGCN" --wandb_run "EMCGCN lap14 D2 bert_lr_1e-5" --config config.yaml --wandb_api_key '10989cf7d666f7d3bfcc31c6185448bda1a19767'
 # CUDA_VISIBLE_DEVICES=0 python main.py --mode test --dataset lap14 --batch_size 16 --epochs 100 --model_dir savemodel/ --seed 1000 --pooling avg --prefix ../data/D2/
